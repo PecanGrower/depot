24e1d617875f94a3545421b60dbf6f93bc2ddfcf
Greg Daviet <Greg@theDaviets.com>
2012-09-06 17:13:00 -0600

    Add time to sidebar

--- spec/requests/store_spec.rb
+++ spec/requests/store_spec.rb
@@ -5,8 +5,10 @@ describe "Store" do
     let!(:all_products) { 3.times { create(:product) } }
     before { visit store_path }
   	it "has the correct page elements" do
+      # time = Time.now.asctime
   		visit store_path
   		expect(page).to have_selector '#columns #side a', minimum: 4
+      expect(page).to have_selector '#time', text: Time.now.asctime
       expect(page).to have_selector '#main .entry', count: 3
       expect(page).to have_selector 'h3', text: Product.first.title
       expect(page).to have_selector '.price', text: /\$[,\d]+\.\d\d/
