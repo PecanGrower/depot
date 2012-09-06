7f8f63b1d5d9d28c131833b7a4f5de869c4730bb
Greg Daviet <Greg@theDaviets.com>
2012-09-06 16:23:27 -0600

    Add integration test for Store#index

--- spec/requests/store_spec.rb
+++ spec/requests/store_spec.rb
@@ -1,10 +1,15 @@
 require 'spec_helper'
 
 describe "Store" do
-  describe "GET /Store" do
-  	it "renders Store#index" do
+  describe "GET :index" do
+    let!(:all_products) { 3.times { create(:product) } }
+    before { visit store_path }
+  	it "has the correct page elements" do
   		visit store_path
-  		expect(page).to have_content 'Catalog'
+  		expect(page).to have_selector '#columns #side a', minimum: 4
+      expect(page).to have_selector '#main .entry', count: 3
+      expect(page).to have_selector 'h3', text: Product.first.title
+      expect(page).to have_selector '.price', text: /\$[,\d]+\.\d\d/
   	end
   end
 end
