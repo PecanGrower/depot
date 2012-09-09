363b0d30f038069d4f78252af5e521c47a1e0537
Greg Daviet <Greg@theDaviets.com>
2012-09-09 11:05:01 -0600

    Refactor carts_spec.rb

--- spec/requests/carts_spec.rb
+++ spec/requests/carts_spec.rb
@@ -3,21 +3,19 @@ require 'spec_helper'
 describe "Carts Pages" do
 
   describe ":show" do
-    # let(:cart) { create(:cart) }
-    let!(:first_product) { create(:product) }
-    let!(:second_product) { create(:product) }
+    let!(:product) { create(:product) }
     before do
       visit store_path
-      click_button first_product.title
+      click_button product.title # redirect_to cart_path(@cart)
+      @line_item = product.line_items.first
     end
+
     it "has the correct elements" do
-      visit cart_path(1)
-      line_item = first_product.line_items.first
       expect(page).to have_selector 'h2', text: 'Your Pragmatic Cart'
-      expect(page).to have_selector "#product_#{first_product.id}",
-                                    text: first_product.title
-      expect(page).to have_selector "#product_#{first_product.id}", 
-                                    text: line_item.quantity.to_s
+      expect(page).to have_selector "#product_#{product.id}",
+                                    text: product.title
+      expect(page).to have_selector "#product_#{product.id}", 
+                                    text: @line_item.quantity.to_s
     end
   end
 end
