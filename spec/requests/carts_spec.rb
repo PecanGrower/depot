cd24593f35aced04e3b17445747744a9f6990be6
Greg Daviet <Greg@theDaviets.com>
2012-09-09 12:19:08 -0600

    Refactor Carts Pages spec

--- spec/requests/carts_spec.rb
+++ spec/requests/carts_spec.rb
@@ -2,20 +2,35 @@ require 'spec_helper'
 
 describe "Carts Pages" do
 
+  subject { page }
+
   describe ":show" do
     let!(:product) { create(:product) }
     before do
       visit store_path
-      click_button product.title # redirect_to cart_path(@cart)
-      @line_item = product.line_items.first
+      click_button product.title
     end
 
-    it "has the correct elements" do
-      expect(page).to have_selector 'h2', text: 'Your Pragmatic Cart'
-      expect(page).to have_selector "#product_#{product.id}",
-                                    text: product.title
-      expect(page).to have_selector "#product_#{product.id}", 
-                                    text: @line_item.quantity.to_s
+    describe "elements" do
+      it { should have_selector 'h2', text: 'Your Pragmatic Cart' }
+    end
+
+    describe "products" do
+      let(:cart) { product.line_items.last.cart } # find current_cart
+      let(:other_product) { create(:product) }
+      before do
+        2.times { cart.add_product(other_product).save }
+        visit current_path # reload cart page
+      end
+
+      it "are listed with quantities" do
+        cart.line_items.each do |item|
+          expect(page).to have_selector "#product_#{item.product.id}",
+                                        text: item.product.title 
+          expect(page).to have_selector "#product_#{item.product.id}",
+                                        text: item.quantity.to_s
+        end
+      end
     end
   end
 end
