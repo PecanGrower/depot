cb39020dfb4f2098f77d7daa566b15774a4dc96a
Greg Daviet <Greg@theDaviets.com>
2012-09-09 11:24:55 -0600

    Add Product.price test to store_spec and refactor

--- spec/requests/store_spec.rb
+++ spec/requests/store_spec.rb
@@ -2,25 +2,28 @@ require 'spec_helper'
 
 describe "Store" do
   let!(:all_products) { 3.times { create(:product) } }
+  before { visit store_path }
 
   describe "catalog listing" do
-    before { visit store_path }
   	it "has the correct page elements" do
   		visit store_path
   		expect(page).to have_selector '#columns #side a', minimum: 4
       expect(page).to have_selector '#time'
       expect(page).to have_selector '#main .entry', count: 3
+  	end
+
+    it "lists all of the products" do
       Product.all.each do |product|
         expect(page).to have_selector 'h3', text: product.title
+        expect(page).to have_selector '.price', text: /\$[,\d]+\.\d\d/
+        expect(page).to have_selector '.price', text: product.price.to_s
       end
-      expect(page).to have_selector '.price', text: /\$[,\d]+\.\d\d/
-  	end
+    end
   end
 
   describe "Add to Cart button" do
     
     it "displays a flash:notice " do
-      visit store_path
       click_button 'Add'
       expect(page).to have_selector '#notice'
     end
