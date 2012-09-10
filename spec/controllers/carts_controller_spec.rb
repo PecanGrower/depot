7b14d0b3469b6b6971efc4d6714556482e7b9331
Greg Daviet <Greg@theDaviets.com>
2012-09-09 22:14:05 -0600

    Refactor carts_controller

--- spec/controllers/carts_controller_spec.rb
+++ spec/controllers/carts_controller_spec.rb
@@ -1,13 +1,22 @@
 require 'spec_helper'
 
 describe CartsController do
+  let!(:cart) { create(:cart) }
+  let!(:any_cart) { create(:cart) } # ensure @cart is always set to current_cart
+  before { session[:cart_id] = cart.id } # set current_cart to cart
+
+  describe "GET :show" do
+    
+    it "assign current_cart to @cart" do
+      get :show, id: any_cart
+      expect(assigns(:cart).id).to eq cart.id
+    end
+  end
 
   describe "DELETE :destroy" do
-    let!(:cart) { create(:cart) }
-    before { session[:cart_id] = cart.id }
 
     it "assigns current_cart to @cart" do
-      delete :destroy, id: cart
+      delete :destroy, id: any_cart
       expect(assigns(:cart).id).to eq session[:cart_id]
     end
 
@@ -16,35 +25,33 @@ describe CartsController do
     end
   end
 
+  # current_cart is an inherrited private method from application_controller
   describe "#current_cart method" do
-    # current_cart is an inherrited private method from application_controller
     
     context "when there is no current_cart" do
       before { session[:cart_id] = nil }
 
       it "creates a new cart" do
         expect do
-          post :show, id: 1
+          post :show, id: any_cart
         end.to change(Cart, :count).by(1)
       end
 
       it "assigns current_cart.id to session[:cart_id]" do
-        post :show, id: 1
+        post :show, id: any_cart # creates a new cart
         expect(session[:cart_id]).to eq Cart.last.id
       end
 
       it "assigns the current_cart to @cart" do
-        post :show, id: 1
+        post :show, id: any_cart
         expect(assigns(:cart).id).to eq session[:cart_id]
       end
     end
 
     context "when there is a current_cart" do
-      let(:cart) { create(:cart) }
-      before { session[:cart_id] = cart.id }
 
       it "assigns the current_cart to @cart" do
-        post :show, id: 1
+        post :show, id: any_cart
         expect(assigns(:cart).id).to eq cart.id
       end
     end
