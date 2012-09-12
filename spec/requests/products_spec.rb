3840335fecb9d803fd37c228ef7fa1d2fa2ccc72
Greg Daviet <Greg@theDaviets.com>
2012-09-12 08:35:59 -0600

    Add integration test for products page

--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -2,4 +2,11 @@ require 'spec_helper'
 
 describe "Products" do
 
+  subject { page }
+
+  describe ":index" do
+    before { visit products_path }
+
+    it { should have_selector 'h1', text: 'Listing products'}
+  end
 end
